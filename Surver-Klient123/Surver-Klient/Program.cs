2f790a37bdd3aa62ccb009acc7d097dc8247c774
unknown <KristianValkov>
2023-10-27 10:08:38 +0300

    first commit

---
+++ Surver-Klient123/Surver-Klient/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Surver_Klient
+{
+    internal class Program
+    {
+        public class ChatClinet
+        {
+            private TcpClient client;
+            private StreamReader reader;
+            private StreamWriter writer;
+            public void Start()
+            {
+                client = new TcpClient("127.0.0.1", 12345);
+                NetworkStream stream = client.GetStream();
+                reader = new StreamReader(stream);
+                writer = new StreamWriter(stream);
+                Thread receiveThread = new Thread(Receive);
+                receiveThread.Start();
+                Thread sendThread = new Thread(Receive);
+
+                sendThread.Start();
+            }
+            private void Receive()
+            {
+                while (true)
+                {
+                    try
+                    {
+                        string message = reader.ReadLine();
+                        Console.WriteLine("Сървър" + message);
+                    }
+                    catch (Exception es)
+                    {
+                        Console.WriteLine("Error" + es.Message);
+                        break;
+                    }
+                }
+                //try
+                //{
+                //    string message = reader.ReadLine();
+                //    Console.WriteLine("Сървър");
+                //}
+                //catch (Exception)
+                //{
+
+                //    throw;
+                //}
+            }
+            private void Send()
+            {
+                while (true)
+                {
+                    try
+                    {
+                        string message = reader.ReadLine();
+                        Console.WriteLine("Клиент" + message);
+                        writer.Flush();
+                    }
+                    catch (Exception es)
+                    {
+                        Console.WriteLine("Error" + es.Message);
+                        break;
+                    }
+                }
+            }
+        }
+
+        static void Main(string[] args)
+        {
+           
+        }
+    }
+}
---
+++ Качествен код/Код/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Код
+{
+    internal class Program
+    {
+        public const string FILLE_NAME = "example.bin";
+        static void Main(string[] args)
+        {
+            FileStream fs = new FileStream(FILLE_NAME, FileMode.CreateNew);
+            BinaryWriter bw = new BinaryWriter(fs);
+            BinaryReader br = new BinaryReader(fs);
+            using (fs)
+            {
+                for (int i = 0; i < 11; i++)
+                {
+                    bw.Write((int)i);
+                }
+                for (int i = 0; i<11;i++)
+                {
+                    fs = new FileStream(FILLE_NAME, FileMode.Open, FileAccess.Read);
+                    Console.WriteLine(br.ReadInt32());
+                }
+                //br.Close();  //fs.Close();
+            }
+        }
+    }
+}
